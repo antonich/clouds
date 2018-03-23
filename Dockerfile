dfd5cfd153a1ef6004ccc74bf8f3dd4c89144e0a
Anton Zinchenko <antonich.s.b.m@gmail.com>
2018-03-23 13:35:18 +0100

    with heroku without docker

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,10 @@ WORKDIR /code
 ADD requirements.txt /code/
 RUN pip install -r requirements.txt
 ADD . /code/
-CMD gunicorn --bind 0.0.0.0:$PORT wsgi
+
+# EXPOSE port 8000 to allow communication to/from server
+EXPOSE 8000
+
+# CMD specifcies the command to execute to start the server running.
+CMD ["/start.sh"]
+# done!
--- clouds/settings.py
+++ clouds/settings.py
@@ -64,16 +64,22 @@ WSGI_APPLICATION = 'clouds.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
